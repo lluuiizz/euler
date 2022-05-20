fd538228205c09202dda8bd963165d91b78f4e4f
Luiz Guilherme Costa da Silva <luiz.developer.acc@gmail.com>
2022-05-19 21:10:48 -0300

    new problem solved: 5!!!

--- src/problem5.rs
+++ src/problem5.rs
@@ -1,23 +1,51 @@
-fn main() {
-    mmc_1_to_10();
+const RANGE: usize = 50;
 
+fn main() {
+    let mut nums: [i32; RANGE] = [0; RANGE];
+    let mut curr_mmc = 1;
+    let mut divisor = 2;
+
+    for i in 0..nums.len() {
+        nums[i] = 1 + i as i32;
+    }
+
+   while !is_all_one(nums) {
+        if can_divide_something(nums, divisor) {
+            for i in 0..nums.len() {
+                nums[i] = 
+                    if nums[i] % divisor == 0 {
+                        nums[i] / divisor
+                } 
+                    else {
+                        nums[i]    
+                }
+            }
+            curr_mmc = curr_mmc * divisor;
+        }
+        else {
+            divisor += 1;
+        }
+   }
+
+   println!("mmc is {}", curr_mmc);
 }
 
-
-fn mmc_1_to_10 ()   {
-    let mut nums: [u8; 20] = [0; 20];
-    let mut current_divisor = 2;
-    for i in 0..20 {
-        nums[i] += 1 + i as u8;
+fn can_divide_something (nums: [i32; RANGE], divisor: i32) -> bool {
+    for num in nums {
+        if num % divisor == 0 {
+            return true;
+        }
     }
 
-    for i in 0..20 {
-       if nums[i] % current_divisor == 0 {
-            nums[i] = nums[i] / current_divisor;
-       } 
+    return false;
+}
+
+fn is_all_one (nums: [i32; RANGE]) -> bool {
+    for num in nums {
+        if num != 1 {
+            return false;
+        }
     }
 
-    for i in nums {
-        println!("{}", i);
-    }
+    return true;
 }
